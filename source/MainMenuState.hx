8fad07a3a1269d9ef9b6606e5e5de64fa4fc70d8
DandeeGanteng <110116385+DandeeGanteng@users.noreply.github.com>
2024-08-18 04:43:24 +0700

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,7 +35,7 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
+	         'awards',
 		'credits',
 		'options'
 	];
